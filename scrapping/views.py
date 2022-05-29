d489f28d6ab908d375880ad58e19e052255738d2
aninda052 <anindahalder052@gmail.com>
2022-05-29 19:44:18 +0600

    landing page view added with template and style

--- scrapping/views.py
+++ scrapping/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+
+
+class LandingPageView(View):
+
+    def get(self, request):
+        return render(request, 'landing.html')
+
+    def post(self, request):
+        return render(request, 'landing.html')
+
+
+
