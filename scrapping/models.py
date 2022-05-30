0d38f2c5279191ef44de524b79271ee0cd106c1f
aninda052 <anindahalder052@gmail.com>
2022-05-30 12:57:37 +0600

    model for scrapped image created

--- scrapping/models.py
+++ scrapping/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class ScrappedImage(models.Model):
+    def image_path(self, filename):
+        return f"images/{self.domain}/{filename}"
+
+    image = models.ImageField(null=False, upload_to=image_path)
+    image_url = models.URLField(unique=True, null=False)
+    scraped_url = models.URLField(null=False)
+    domain = models.CharField(max_length=25)
+    download_date = models.DateField(auto_now_add=True)
+    image_dimension = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.image_url
+
+
+
+
+
