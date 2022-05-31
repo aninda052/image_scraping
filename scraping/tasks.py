50a469f1e3b78880a214c16a718500fe9108cdf6
aninda052 <anindahalder052@gmail.com>
2022-05-31 22:14:10 +0600

    background task modify
    instead of creating background task for each image url, create one single task for full batch

--- scraping/tasks.py
+++ scraping/tasks.py
@@ -6,20 +6,23 @@ from .models import ScrapedImage
 
 
 @background()
-def fetch_image_and_save(scheme, domain, image_source, scrapping_url):
-    if domain not in image_source:
-        image_source = f'{scheme}://{domain}{image_source}'
+def fetch_image_and_save(image_source_list, scheme, domain, scrapping_url):
 
-    file_name = image_source.split('/')[-1]
+    for image_source in image_source_list:
 
+        if domain not in image_source:
+            image_source = f'{scheme}://{domain}{image_source}'
 
-    response = requests.get(image_source)
+        if not ScrapedImage.objects.filter(image_source=image_source).exists():
+            file_name = image_source.split('/')[-1]
 
-    if response.status_code == 200:
-        scrapped_image_obj = ScrapedImage()
-        scrapped_image_obj.image_source = image_source
-        scrapped_image_obj.scraped_url = scrapping_url
-        scrapped_image_obj.domain = domain
+            response = requests.get(image_source)
 
-        scrapped_image_obj.image_original.save(file_name, ContentFile(response.content), save=False)
-        scrapped_image_obj.save()
+            if response.status_code == 200:
+                scrapped_image_obj = ScrapedImage()
+                scrapped_image_obj.image_source = image_source
+                scrapped_image_obj.scraped_url = scrapping_url
+                scrapped_image_obj.domain = domain
+
+                scrapped_image_obj.image_original.save(file_name, ContentFile(response.content), save=False)
+                scrapped_image_obj.save()
--- scraping/views.py
+++ scraping/views.py
@@ -4,7 +4,6 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
-from .models import ScrapedImage
 from .tasks import fetch_image_and_save
 
 class LandingPageView(View):
@@ -25,19 +24,13 @@ class LandingPageView(View):
             htmldata = requests.get(scrapping_url).text
             soup = BeautifulSoup(htmldata, 'html.parser')
 
-            scrapped_image_count = 0
-            for item in soup.find_all('img'):
-                image_source = ""
-                if 'src' in item.attrs:
-                    image_source = item['src']
 
-                if not image_source or 'svg' in image_source.split('.')[-1]:
-                    continue
-                if not ScrapedImage.objects.filter(image_source=image_source).exists():
-                    fetch_image_and_save(scheme, domain,image_source, scrapping_url)
-                    scrapped_image_count += 1
+            image_source_list = [item['src'] for item in soup.find_all('img')
+                                 if 'src' in item.attrs and item['src']  and 'svg' not in item['src'].split('.')[-1]]
 
-            massage = f'Total {scrapped_image_count} images found'
+            fetch_image_and_save(image_source_list, scheme, domain, scrapping_url)
+
+            massage = f'Total {len(image_source_list)} images found'
 
         context = {"massage" : massage}
         return render(request, 'landing.html', context=context)
