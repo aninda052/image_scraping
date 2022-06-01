602298313cbdaac19bf38854ec7cb29cf9bc17a0
aninda052 <anindahalder052@gamil.com>
2022-06-02 01:27:35 +0600

    validate image url before fetching

--- scraping/tasks.py
+++ scraping/tasks.py
@@ -1,23 +1,26 @@
+import os
+
 from background_task import background
 import requests
 from django.core.files.base import ContentFile
 from multiprocessing import Pool, cpu_count
 from .models import ScrapedImage
+import validators
 
-
-@background()
+@background(schedule=5)
 def fetch_image_and_save(image_source_list, scheme, domain, scrapping_url):
 
     fetch_image_function_arg_list = []
     for image_source in image_source_list:
 
-        if domain not in image_source:
-            image_source = f'{scheme}://{domain}{image_source}'
+        # if domain not in image_source:
+        #     image_source = f'{scheme}://{domain}{image_source}'
 
         if not ScrapedImage.objects.filter(image_source=image_source).exists():
             file_name = f"{image_source.split('/')[-1].split('.')[0]}.jpeg"
 
             fetch_image_function_arg_list.append([image_source, scrapping_url, domain, file_name])
+            fetch_image(image_source, scrapping_url, domain, file_name)
 
     pool = Pool(processes=cpu_count()-1)
     pool.map(fetch_image_wrapper, fetch_image_function_arg_list)
@@ -28,16 +31,16 @@ def fetch_image_wrapper(args):
     fetch_image(*args)
 
 def fetch_image(image_source, scrapping_url, domain, file_name):
+    # print(os.getpid(), image_source)
 
+    if validators.url(image_source):
+        response =  requests.get(image_source)
+        if response.status_code == 200:
+            scrapped_image_obj = ScrapedImage()
+            scrapped_image_obj.image_source = image_source
+            scrapped_image_obj.scraped_url = scrapping_url
+            scrapped_image_obj.domain = domain
 
-
-    response =  requests.get(image_source)
-    if response.status_code == 200:
-        scrapped_image_obj = ScrapedImage()
-        scrapped_image_obj.image_source = image_source
-        scrapped_image_obj.scraped_url = scrapping_url
-        scrapped_image_obj.domain = domain
-
-        scrapped_image_obj.image_original.save(file_name, ContentFile(response.content), save=False)
-        scrapped_image_obj.save()
+            scrapped_image_obj.image_original.save(file_name, ContentFile(response.content), save=False)
+            scrapped_image_obj.save()
 
